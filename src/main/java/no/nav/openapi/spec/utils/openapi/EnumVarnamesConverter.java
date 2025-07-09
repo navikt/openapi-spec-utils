410f16526ab45a35cc373b0adcec76b900e2e7c4
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-07-09 09:26:50 +0200

    Removed use of separate objectmapper in EnumVarnamesConverter and TimeTypesModelConverter.
    Realized that we already had the required type data available, so don't need to resolve it via objectmapper.

--- src/main/java/no/nav/openapi/spec/utils/openapi/EnumVarnamesConverter.java
+++ src/main/java/no/nav/openapi/spec/utils/openapi/EnumVarnamesConverter.java
@@ -1,7 +1,6 @@
 package no.nav.openapi.spec.utils.openapi;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.SimpleType;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
 import io.swagger.v3.core.converter.ModelConverterContext;
@@ -17,32 +16,27 @@ import java.util.Iterator;
  * med samme verdi som java kode har definert for enum verdiane.
  */
 public class EnumVarnamesConverter implements ModelConverter {
-    private final ObjectMapper objectMapper;
-
-    public EnumVarnamesConverter(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @Override
     public Schema<?> resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
         if(chain.hasNext()) {
             // First, resolve the schema for current type
             final var resolvedSchema = chain.next().resolve(type, context, chain);
             // If resolved schema type has enum values, resolve and add x-enum-varnames
-            if(resolvedSchema != null && resolvedSchema.getEnum() != null && !resolvedSchema.getEnum().isEmpty()) {
-                // Find enum name values, and add them to "x-enum-varnames" extension.
-                // This makes the generated enum consts have the same property names as the java enums has declared.
-                final var enumValues = resolvedSchema.getEnum();
-                final JavaType javaType = objectMapper.constructType(type.getType());
-                if (javaType != null && javaType.isEnumType()) {
-                    final Class<Enum> enumClass = (Class<Enum>) javaType.getRawClass();
+            if(resolvedSchema != null && type.getType() instanceof SimpleType simpleType && simpleType.isEnumType()) {
+                final var createdEnumSchema = context.resolve(type);
+                if(createdEnumSchema != null && createdEnumSchema.getEnum() != null && !createdEnumSchema.getEnum().isEmpty()) {
+                    // Find enum name values, and add them to "x-enum-varnames" extension.
+                    // This makes the generated enum consts have the same property names as the java enums has declared.
+                    final var enumValues = createdEnumSchema.getEnum();
+                    final Class<Enum> enumClass = (Class<Enum>) simpleType.getRawClass();
                     final String[] names = Arrays.stream(enumClass.getEnumConstants()).map(Enum::name).toArray(String[]::new);
                     if(names.length != enumValues.size()) {
                         throw new RuntimeException("resolved enum values count ("+enumValues.size()+") not equal constructed enum class constants count ("+names.length+")");
                     }
-                    resolvedSchema.addExtension("x-enum-varnames", names);
+                    createdEnumSchema.addExtension("x-enum-varnames", names);
                 }
             }
+
             return resolvedSchema;
         } else {
             return null;
